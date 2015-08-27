923dd0595e8adf44e7b4afb0b03bad1352d1adf3
Lucas Leblow <leblowl@gmail.com>
2015-08-28 01:40:33 +0200

    removed unused datasets, indexes, & functions for now

--- setup/functions.sql
+++ setup/functions.sql
@@ -21,97 +21,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION mz_calculate_poi_level(
-    aerialway_val text,
-    aeroway_val text,
-    amenity_val text,
-    barrier_val text,
-    highway_val text,
-    historic_val text,
-    leisure_val text,
-    lock_val text,
-    man_made_val text,
-    natural_val text,
-    power_val text,
-    railway_val text,
-    shop_val text,
-    tourism_val text,
-    waterway_val text
-)
-RETURNS SMALLINT AS $$
-BEGIN
-    RETURN (
-        CASE WHEN aeroway_val IN ('aerodrome', 'airport') THEN 9
-             WHEN natural_val IN ('peak', 'volcano') THEN 11
-             WHEN railway_val IN ('station') THEN 12
-             WHEN (aerialway_val IN ('station')
-                   OR railway_val IN ('halt', 'tram_stop')
-                   OR tourism_val IN ('alpine_hut', 'zoo')) THEN 13
-             WHEN (natural_val IN ('spring')
-                   OR railway_val IN ('level_crossing')) THEN 14
-             WHEN (amenity_val IN ('hospital')
-                   OR barrier_val IN ('gate')
-                   OR highway_val IN ('gate', 'mini_roundabout')
-                   OR lock_val IN ('yes')
-                   OR man_made_val IN ('lighthouse', 'power_wind')
-                   OR natural_val IN ('cave_entrance')
-                   OR power_val IN ('generator')
-                   OR waterway_val IN ('lock')) THEN 15
-             WHEN (aeroway_val IN ('helipad')
-                   OR amenity_val IN ('biergarten', 'bus_station', 'car_sharing',
-                                      'picnic_site', 'place_of_worship',
-                                      'prison', 'pub', 'recycling', 'shelter')
-                   OR barrier_val IN ('block', 'bollard', 'lift_gate')
-                   OR highway_val IN ('ford')
-                   OR historic_val IN ('archaeological_site')
-                   OR man_made_val IN ('windmill')
-                   OR natural_val IN ('tree')
-                   OR shop_val IN ('department_store', 'supermarket')
-                   OR tourism_val IN ('camp_site', 'caravan_site', 'information', 'viewpoint')) THEN 16
-             WHEN (aeroway_val IN ('gate')
-                   OR amenity_val IN (
-                 'atm', 'bank', 'bar', 'bicycle_rental', 'bicycle_parking', 'bus_stop',
-                 'cafe', 'cinema', 'courthouse', 'drinking_water', 'embassy', 'emergency_phone',
-                 'fast_food', 'fire_station', 'fuel', 'library', 'parking', 'pharmacy',
-                 'police', 'post_box', 'post_office', 'restaurant', 'telephone', 'theatre',
-                 'toilets', 'veterinary')
-                   OR highway_val IN ('bus_stop', 'traffic_signals')
-                   OR historic_val IN ('memorial')
-                   OR leisure_val IN ('playground', 'slipway')
-                   OR man_made_val IN ('mast', 'water_tower')
-                   OR shop_val IN ('bakery', 'bicycle', 'books', 'butcher', 'car', 'car_repair',
-                                 'clothes', 'computer', 'convenience',
-                                 'doityourself', 'dry_cleaning', 'fashion', 'florist', 'gift',
-                                 'greengrocer', 'hairdresser', 'jewelry', 'mobile_phone',
-                                 'optician', 'pet')
-                   OR tourism_val IN ('bed_and_breakfast', 'chalet', 'guest_house',
-                                    'hostel', 'hotel', 'motel', 'museum')
-                   OR railway_val IN ('subway_entrance')) THEN 17
-             WHEN (amenity_val IN ('bench', 'waste_basket')) THEN 18
-             ELSE NULL END
-    );
-END;
-$$ LANGUAGE plpgsql IMMUTABLE;
-
-CREATE OR REPLACE FUNCTION mz_calculate_road_level(highway_val text, railway_val text, aeroway_val text, network_val text)
-RETURNS SMALLINT AS $$
-BEGIN
-    RETURN (
-        CASE WHEN (highway_val IN ('motorway', 'trunk', 'primary', 'motorway_link') OR network_val='US:I') THEN 9
-             WHEN highway_val IN ('secondary') THEN 10
-             WHEN (highway_val IN ('tertiary')
-                OR aeroway_val IN ('runway', 'taxiway')) THEN 11
-             WHEN highway_val IN ('trunk_link', 'residential', 'unclassified', 'road') THEN 12
-             WHEN highway_val IN ('primary_link', 'secondary_link') THEN 13
-             WHEN (highway_val IN ('tertiary_link', 'minor')
-                OR railway_val='rail') THEN 14
-             WHEN (highway_val IN ('service', 'footpath', 'track', 'footway', 'steps', 'pedestrian', 'path', 'cycleway', 'living_street')
-                OR railway_val IN ('tram', 'light_rail', 'narrow_gauge', 'monorail')) THEN 15
-             ELSE NULL END
-    );
-END;
-$$ LANGUAGE plpgsql IMMUTABLE;
-
 CREATE OR REPLACE FUNCTION mz_calculate_is_water(
     waterway_val text, natural_val text, landuse_val text)
 RETURNS BOOLEAN AS $$
@@ -129,16 +38,5 @@ CREATE OR REPLACE FUNCTION mz_calculate_is_building_or_part(
 RETURNS BOOLEAN AS $$
 BEGIN
     RETURN (building_val IS NOT NULL OR buildingpart_val IS NOT NULL);
-END;
-$$ LANGUAGE plpgsql IMMUTABLE;
-
-CREATE OR REPLACE FUNCTION mz_calculate_transit_level(route_val text)
-RETURNS SMALLINT AS $$
-BEGIN
-    RETURN (
-        CASE WHEN route_val IN ('train', 'railway') THEN 6
-             WHEN route_val IN ('subway', 'light_rail', 'tram') THEN 10
-             ELSE NULL END
-    );
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
--- setup/perform-sql-updates.sh
+++ setup/perform-sql-updates.sh
@@ -5,13 +5,12 @@ echo "Creating functions..."
 psql $@ -f functions.sql
 echo "done."
 
-# apply updates in parallel across tables
-echo -e "\nApplying updates in parallel across tables..."
-psql $@ -f apply-updates-non-planet-tables.sql &
-psql $@ -f apply-planet_osm_polygon.sql &
-psql $@ -f apply-planet_osm_line.sql &
-psql $@ -f apply-planet_osm_point.sql &
-wait
+# apply updates
+echo -e "\nApplying updates..."
+psql $@ -f apply-updates-non-planet-tables.sql
+psql $@ -f apply-planet_osm_polygon.sql
+psql $@ -f apply-planet_osm_line.sql
+psql $@ -f apply-planet_osm_point.sql
 echo "done."
 
 echo -e '\nApplying triggers...'
